5e38c6b451b4deef88ee8d9850d455bf03324450
Loïc <loic.risler@gmail.com>
2026-02-10 01:59:20 +0100

    test correction touch_handling

--- public/code/touch_handling.js
+++ public/code/touch_handling.js
@@ -1,26 +1,30 @@
 "use strict"
 
-const logoLink = document.querySelector('.logo-link');
-const logo = document.querySelector('.logo');
-let touched = false;
-let resetTimer = null;
+const logoLinks = document.querySelector('.logo-link');
 
-logoLink.addEventListener('touchstart', function(e){
-    e.preventDefault();
 
-    if (!touched){
-        logo.classList.add('touched');
-        touched = true;
-        resetTimer = setTimeout(function(){
-            logo.classList.remove('touched');
-            touched = false;
-        }, 3000);
-        console.log('resetTimer :>> ', resetTimer);
-        console.log('touched :>> ', touched);
+logoLinks.forEach(logoLink => {
+    const logo = logoLink.querySelector('.logo');
+    let touched = false;
+    let resetTimer = null;
+    
+    logoLink.addEventListener('touchstart', function(e){
+        e.preventDefault();
 
-    } else {
-        clearTimeout(resetTimer);
-        window.location.href = logoLink.getAttribute('href');
-    }
+        if (!touched){
+            logo.classList.add('touched');
+            touched = true;
+            resetTimer = setTimeout(function(){
+                logo.classList.remove('touched');
+                touched = false;
+            }, 3000);
+            console.log('resetTimer :>> ', resetTimer);
+            console.log('touched :>> ', touched);
+
+        } else {
+            clearTimeout(resetTimer);
+            window.location.href = logoLink.getAttribute('href');
+        }
+    });
+
 });
-
